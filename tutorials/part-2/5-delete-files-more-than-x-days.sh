27239061d376b77dde6f90c28e17ae08190476b3
Vignesh Sweekaran <vigneshsweekaran@gmail.com>
2023-05-07 11:33:16 +0800

    updated

--- tutorials/part-2/5-delete-files-more-than-x-days.sh
+++ tutorials/part-2/5-delete-files-more-than-x-days.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PATH="/tmp"
+TARGET_PATH="/tmp"
 TARGET_FOLDERS="log_files", "error_files", "access_files"
 FILE_EXTENSIONS=(".log", ".txt")
 TARGET_DAYS="10"
@@ -11,6 +11,7 @@ for folder in ${TARGET_FOLDERS[@]}
 do
   for extension in ${FILE_EXTENSIONS[@]}
   do
-    find ${PATH}/${folder} -type f -name "*${FILE_EXTENSION}" -mtime +${TARGET_DAYS} -delete
+    echo "Deleting file extension ${extension} in folder ${folder}"
+    find ${TARGET_PATH}/${folder} -type f -name "*${FILE_EXTENSION}" -mtime +${TARGET_DAYS} -delete
   done
 done
