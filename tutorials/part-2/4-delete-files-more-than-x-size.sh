a3a38cd3822bca55cea8fa0f225a078c0f8a631e
Vignesh Sweekaran <vigneshsweekaran@gmail.com>
2023-05-07 11:31:13 +0800

    updated

--- tutorials/part-2/4-delete-files-more-than-x-size.sh
+++ tutorials/part-2/4-delete-files-more-than-x-size.sh
@@ -4,6 +4,6 @@ set -e
 
 TARGET_PATH="/tmp/files"
 FILE_EXTENSION=".log"
-TARGET_FILE_SIZE="1K" #Eg: 10K, 100M, 1GB
+TARGET_FILE_SIZE="1k" #Eg: 10K, 100M, 1GB
 
-find ${PATH} -type f -name "*${FILE_EXTENSION}" -size +${TARGET_FILE_SIZE} -delete
+find ${TARGET_PATH} -type f -name "*${FILE_EXTENSION}" -size +${TARGET_FILE_SIZE} -delete
