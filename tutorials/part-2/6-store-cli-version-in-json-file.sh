4c212b624640a95ec0324b41b254ce56c914f841
Vignesh Sweekaran <vigneshsweekaran@gmail.com>
2023-05-07 19:28:38 +0800

    updtaed

--- tutorials/part-2/6-store-cli-version-in-json-file.sh
+++ tutorials/part-2/6-store-cli-version-in-json-file.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-GIT_VERSION=$(git --version)
-MAVEN_VERSION=$(mvn --version)
-JAVA_VERSION=$(java -version)
-PYTHON_VERSION=$(python --version)
-
+GIT_VERSION=$(git --version | awk '{print $3}')
+MAVEN_VERSION=$(mvn --version | grep "Apache Maven" | awk '{print $3}')
 FILE_NAME="version.json"
 
 
@@ -15,8 +12,6 @@ then
   echo "{}" > $FILE_NAME
   echo "$(jq --arg git_version "$JAVA_VERSION" '. += {"git": $git_version}' $FILE_NAME)" > $FILE_NAME
   echo "$(jq --arg maven_version "$JAVA_VERSION" '. += {"maven": $maven_version}' $FILE_NAME)" > $FILE_NAME
-  echo "$(jq --arg java_version "$JAVA_VERSION" '. += {"java": $java_version}' $FILE_NAME)" > $FILE_NAME
-  echo "$(jq --arg python_version "$JAVA_VERSION" '. += {"python": $python_version}' $FILE_NAME)" > $FILE_NAME
 else
   echo "jq command not found"
 fi
