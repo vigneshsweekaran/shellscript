66eba06c8c218f6cf5ba59f880d64b77ec920e57
Vignesh Sweekaran <vigneshsweekaran@gmail.com>
2023-05-13 06:34:47 +0800

    updated

--- tutorials/part-3/4-read-and-write-to-different-file.sh
+++ tutorials/part-3/4-read-and-write-to-different-file.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-FILE_NAME="/etc/os-release"
+SOURCE_FILE_NAME="/var/log/httpd/access_log"
+
+TARGET_FOLDER=$(date +"%m%d%y-%H%M%S")
+
+mkdir ${TARGET_FOLDER}
 
 while read -r line; 
-do 
-  echo "$line";
-done < $FILE_NAME
+do
+  if [[ "$line" == *Android* ]] || [[ "$line" == *android* ]]; then
+    echo "$line" >> ${TARGET_FOLDER}/android.log
+  elif [[ "$line" == *Windows* ]]; then
+    echo "$line" >> ${TARGET_FOLDER}/windows.log
+  else
+    echo "$line" >> ${TARGET_FOLDER}/others.log
+  fi
+done < ${SOURCE_FILE_NAME}
