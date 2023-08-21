126115e83207cbd6d92e0c5985b8cda31c30f811
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2023-08-21 17:14:12 +0530

    Update main.sh

--- automation/03-install-nginx-ubuntu/main.sh
+++ automation/03-install-nginx-ubuntu/main.sh
@@ -2,4 +2,4 @@
 
 sudo apt update
 sudo apt install -y nginx
-echo "Installed via Azure extensions - custom script" > /var/nginx/www/html/index.html
+echo "Installed via Azure extensions - custom script" > /var/www/html/index.nginx-debian.html
